95f25f496d813f02cbc0cb391cf80af25afd9505
doha <dohaahi@naver.com>
2023-11-23 03:34:25 +0900

    feat: 숫자 입력 받는 기능 구현

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -5,6 +5,13 @@ import baseball.view.InputView;
 
 public class BaseballController {
     public void run() {
+        // 1. 숫자 입력 받음
         Baseball baseball = InputView.readBaseBallNumber();
+
+        // 2. 게임 실행
+
+
+        // 3. 결과 출력
+
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -13,6 +13,7 @@ public class InputView {
 
     public static Baseball readBaseBallNumber() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
 
         String input = readLine();
         validateInputBaseball(input);
