2b503a082dd85a8ef05eb07bbc0029ce2609feae
doha <dohaahi@naver.com>
2023-11-23 15:04:23 +0900

    feat: 숫자 비교하는 결과 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,24 @@ import baseball.domain.MatchDto;
 
 public class OutputView {
     public static void printMatchResult(final MatchDto matchDto) {
+        int ball = matchDto.ball();
+        int strike = matchDto.strike();
 
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        if (ball != 0 && strike != 0) {
+            System.out.printf("%d볼 %d스트라이크", ball, strike);
+            return;
+        }
+
+        if (strike != 0) {
+            System.out.printf("%d스트라이크", strike);
+            return;
+        }
+
+        System.out.printf("%d볼", ball);
     }
 }
